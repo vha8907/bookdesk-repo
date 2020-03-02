6518cdab115c6b9dea2bd630b0906dd655f7f870
Vinicius Hilbert <viniciusha@gmail.com>
2020-03-02 15:53:59 -0300

    project start, model definitions, initial migration

---
+++ bookdesk/bookdesk_api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
